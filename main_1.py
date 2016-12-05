d970d4537768fb583268bd10592d5a46b73b69aa
Tangi MALLEGOL <tangi.mallegol@gmail.com>
2016-12-05 10:49:18 +0100

    Premier test requête

---
+++ main_1.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 28 09:29:48 2016
+
+@author: guish
+"""
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+import nltk
+
+sentence = """Where is Paris ?"""
+tokens = nltk.word_tokenize(sentence)
+tagged = nltk.pos_tag(tokens)
+
+for element in tagged:
+    
+    if element[1] == "NNP":
+        
+        subject = element[0]
+        print subject
+
+
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql.setQuery("""
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX dc: <http://purl.org/dc/elements/1.1/>
+    PREFIX : <http://dbpedia.org/resource/>
+    PREFIX dbpedia2: <http://dbpedia.org/property/>
+    PREFIX dbpedia: <http://dbpedia.org/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    
+
+    SELECT *
+    WHERE { <http://dbpedia.org/resource/"""+ subject+ """> ?r ?p }
+""")
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+for result in results["results"]["bindings"]:
+    
+    print result
+    
+    #if result["r"]["value"] == "http://dbpedia.org/ontology/wikiPageRedirects":
+        
+        #print(result["p"]["value"])
